44dac5efe47e6f694bd2260b7e213cdc1adb68d4
KatrevichA <katrevich24@gmail.com>
2024-06-08 06:34:52 +0300

    add file to hw 9.1

--- src/pages/CarsPage.tsx
+++ src/pages/CarsPage.tsx
@@ -1,22 +1,41 @@
 import React, {useEffect, useState} from 'react';
 import {carService} from "../services/api.service";
 import CarsComponent from "../components/CarsComponent";
-import {ICarWithAuthModel} from "../models/ICarWithAuthModel";
+import {ICarPaginatedModel} from "../models/ICarPaginatedModel";
+import PaginationComponent from "../components/PaginationComponent";
 
 const CarsPage = () => {
 
-    const [cars, setCars] = useState<ICarWithAuthModel[]>([]);
+    const [carsPaginatedObject, setCarsPaginatedObject] = useState<ICarPaginatedModel>({
+        items: [],
+        next: null,
+        prev: null,
+        total_items: 0,
+        total_pages: 0
+    });
     useEffect(() => {
         carService.getCars().then(value => {
-            if (value) {
-                setCars([...value.items]);
+            if (value){
+                setCarsPaginatedObject(value);
             }
         });
     }, []);
 
+    const changePage = (action: string) => {
+        switch (action){
+            case 'prev':
+        //         todo set param
+                break;
+                case 'next':
+                    // todo set param
+                    break;
+        }
+    }
+
     return (
         <div>
-            <CarsComponent cars={cars}/>
+            <CarsComponent cars={carsPaginatedObject.items}/>
+            <PaginationComponent changePage={changePage} next={carsPaginatedObject.next} prev={carsPaginatedObject.prev}/>
         </div>
     );
 };
